6efb6ec6e6681ddd4ed8e9b3ef381ae2a4f4ffe0
Allan <allan.mendes0976@gmail.com>
2025-06-13 12:53:37 -0300

    feat: implement save method in ProdutoService

--- gestao-vendas/src/main/java/com/vendas/gestao_vendas/controlador/ProdutoControlador.java
+++ gestao-vendas/src/main/java/com/vendas/gestao_vendas/controlador/ProdutoControlador.java
@@ -5,11 +5,9 @@ import com.vendas.gestao_vendas.servico.ProdutoServico;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -32,5 +30,12 @@ public class ProdutoControlador {
     public ResponseEntity<Optional<Produto>> buscarPorCodigo(@PathVariable Long codigoCategoria, @PathVariable Long codigo) {
         Optional<Produto> produto = produtoServico.buscarPorCodigo(codigo, codigoCategoria);
         return produto.isPresent() ? ResponseEntity.ok(produto) : ResponseEntity.notFound().build();
+    }
+
+    @ApiOperation(value = "Salvar", nickname = "salvarProduto")
+    @PostMapping
+    public ResponseEntity<Produto> salvar(@RequestBody Produto produto) {
+        Produto produtoSalvo = produtoServico.salvar(produto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(produtoSalvo);
     }
 }
