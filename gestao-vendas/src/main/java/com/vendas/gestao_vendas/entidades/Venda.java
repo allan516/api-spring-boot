4f531a8a0404766eddf4506355e11b28d130b499
Allan <allan.mendes0976@gmail.com>
2025-07-11 21:44:10 -0300

    feat: create Venda and ItemVenda entities

---
+++ gestao-vendas/src/main/java/com/vendas/gestao_vendas/entidades/Venda.java
@@ -0,0 +1,58 @@
+package com.vendas.gestao_vendas.entidades;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "venda")
+public class Venda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo")
+    private Long codigo;
+
+    @Column(name = "data")
+    private LocalDate data;
+
+    @ManyToOne
+    @JoinColumn(name = "codigo_cliente", referencedColumnName = "codigo")
+    private Cliente cliente;
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Venda)) return false;
+        Venda venda = (Venda) o;
+        return Objects.equals(codigo, venda.codigo) && Objects.equals(data, venda.data) && Objects.equals(cliente, venda.cliente);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo, data, cliente);
+    }
+}
